f4eae9f1c8a82a79f7e4ccd240310fe8e494ca2a
Delali34 <ernestanyomitse752@gmail.com>
2024-11-22 14:49:48 +0000

    update

--- app/custom/page.js
+++ app/custom/page.js
@@ -24,13 +24,13 @@ const CustomLogoTies = () => {
 
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center lg:px-4">
           <motion.h1
-            className="text-2xl md:text-4xl lg:text-5xl font-bold mb-4"
+            className="text-xl md:text-2xl lg:text-3xl font-bold mb-4"
             initial="hidden"
             animate="visible"
             variants={fadeInUpVariants}
             transition={{ duration: 0.6, delay: 0.4 }}
           >
-            Custom Logo Ties
+            Custom Logo Ties, Branded Scarves & Accessories
           </motion.h1>
           <motion.p
             className="text-sm md:text-lg lg:text-xl"
@@ -44,6 +44,52 @@ const CustomLogoTies = () => {
             to outfit a team. Custom tie tags are also available to add a brand
             statement or special message.
           </motion.p>
+        </div>
+      </div>
+      <div className="max-w-4xl mx-auto px-4 py-16 grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div>
+          <motion.h2
+            className="text-3xl font-bold mb-6"
+            initial="hidden"
+            animate="visible"
+            variants={fadeInUpVariants}
+            transition={{ duration: 0.6, delay: 0.2 }}
+          >
+            Custom Logo Ties
+          </motion.h2>
+          <motion.p
+            className="text-lg mb-8"
+            initial="hidden"
+            animate="visible"
+            variants={fadeInUpVariants}
+            transition={{ duration: 0.6, delay: 0.4 }}
+          >
+            We create branded tie designs incorporating logo pattern repeats,
+            single placements or full artwork prints with all the sizes needed
+            to outfit a team. Custom tie tags are also available to add a brand
+            statement or special message.
+          </motion.p>
+          <motion.a
+            href=""
+            className="bg-red-500 hover:bg-red-600 text-white py-3 px-6 rounded-md inline-block"
+            initial="hidden"
+            animate="visible"
+            variants={fadeInUpVariants}
+            transition={{ duration: 0.6, delay: 0.6 }}
+          >
+            Custom Logo Ties
+          </motion.a>
+        </div>
+        <div>
+          <motion.img
+            src="/image (2).jpeg"
+            alt="Custom Bow Tie"
+            className="w-full h-full object-cover"
+            initial="hidden"
+            animate="visible"
+            variants={fadeInUpVariants}
+            transition={{ duration: 0.6, delay: 0.2 }}
+          />
         </div>
       </div>
 
