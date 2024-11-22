fadb3a558d4209b56cca08d497777ce3bc32c1c2
Delali34 <ernestanyomitse752@gmail.com>
2024-11-22 14:29:14 +0000

    done

--- app/custom/page.js
+++ app/custom/page.js
@@ -24,7 +24,7 @@ const CustomLogoTies = () => {
 
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center lg:px-4">
           <motion.h1
-            className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4"
+            className="text-2xl md:text-4xl lg:text-5xl font-bold mb-4"
             initial="hidden"
             animate="visible"
             variants={fadeInUpVariants}
